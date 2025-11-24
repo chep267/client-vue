4e95c87d18de954292d7660825b1f69cabd65679
dongntd <dongntd267@gmail.com>
2025-11-24 15:36:46 +0700

    dongntd: update vue

--- src/modules/module-auth/hooks/useSignout.ts
+++ src/modules/module-auth/hooks/useSignout.ts
@@ -11,11 +11,8 @@ import Cookies from 'js-cookie';
 
 /** constants */
 import { AppKey } from '@module-base/constants/AppKey';
-import { AppNotifyColor } from '@module-base/constants/AppNotifyColor';
-import { AuthLanguage } from '@module-auth/constants/AuthLanguage';
 
 /** stores */
-import { useNotifyStore } from '@module-base/stores/useNotifyStore';
 import { useAuthStore } from '@module-auth/stores/useAuthStore';
 
 /** services */
@@ -23,7 +20,6 @@ import { authService } from '@module-auth/services';
 
 export function useSignout() {
     const { push } = useRouter();
-    const notifyStore = useNotifyStore();
     const authStore = useAuthStore();
     const uid = Cookies.get(AppKey.uid) || '';
 
@@ -32,9 +28,6 @@ export function useSignout() {
         onSettled: () => {
             authStore.signout();
             push('/').then();
-        },
-        onError: () => {
-            notifyStore.show({ color: AppNotifyColor.error, messageIntl: AuthLanguage.notify.server.error });
         },
     });
 }
